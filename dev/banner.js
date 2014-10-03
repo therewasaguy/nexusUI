c74b3bd75107b1e9630558aa0ec77390d9267660
Ben Taylor <taylorbf@gmail.com>
2014-10-03 11:17:56 -0500

    Drop -- safari audio context, monospace font

--- dev/banner.js
+++ dev/banner.js
@@ -11,7 +11,7 @@ function banner(target, transmitCommand) {
 					
 	//self awareness
 	var self = this;
-	this.defaultSize = { width: 125, height: 50 };
+	this.defaultSize = { width: 100, height: 50 };
 	
 	//get common attributes and methods
 	getTemplate(self, target, transmitCommand);
